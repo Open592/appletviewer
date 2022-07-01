9851164f99c7ad00b10b63713dd978ee4d805ba8
Open592 Developer <uss@open592.com>
2022-06-30 23:22:55 -0700

    debug/capture: Add multi message test case
    Signed-off-by: Open592 Developer <uss@open592.com>

--- src/test/kotlin/com/open592/debug/capture/OutputCaptureTest.kt
+++ src/test/kotlin/com/open592/debug/capture/OutputCaptureTest.kt
@@ -5,16 +5,35 @@ import kotlin.test.assertEquals
 
 class OutputCaptureTest {
     @Test
-    fun singleCaptureTest() {
-        val message = "test"
+    fun singleMessageTest() {
+        val input = "test"
         val expected = "test\n"
         val capture = OutputCapture()
 
-        println(message)
+        println(input)
 
         val messages: List<String> = capture.getOut()
 
         assertEquals(0, capture.getErr().size, "Expected StdErr list to not contain any values")
+        assertEquals(1, messages.size, "Expected StdOut to have one value")
+
+        assertEquals(expected, messages[messages.lastIndex])
+    }
+
+    @Test
+    fun multipleMessageTest() {
+        val inputs = arrayOf("test", "one", "two", "three")
+        val expected = "${inputs.joinToString("")}\n"
+        val capture = OutputCapture()
+
+        inputs.forEach {
+            print(it)
+        }
+
+        println()
+
+        val messages: List<String> = capture.getOut()
+
         assertEquals(1, messages.size, "Expected StdOut to have one value")
 
         assertEquals(expected, messages[messages.lastIndex])
