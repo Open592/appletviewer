3e51f93d79ec04c962a7173f90733b9fa7868a9b
Open592 Developer <uss@open592.com>
2022-07-29 02:11:15 -0700

    all: Update dependencies and cleanup plugins
    Signed-off-by: Open592 Developer <uss@open592.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     base
     application
-    kotlin("jvm")
+    alias(libs.plugins.kotlin.jvm)
 
     alias(libs.plugins.kotlinter)
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,11 +1,5 @@
 
 rootProject.name = "appletviewer"
 
-pluginManagement {
-    plugins {
-        kotlin("jvm") version "1.6.21"
-    }
-}
-
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 enableFeaturePreview("VERSION_CATALOGS")
