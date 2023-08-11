601c85125ff22a8ec6060e819f7d439dff7b88a0
Open592 Developer <uss@open592.com>
2023-08-11 00:46:02 -0700

    Environment: Add initial implementation for Environment
    This will allow for detecting OS and Arch for browsercontrol resolution.
    
    Signed-off-by: Open592 Developer <uss@open592.com>

---
+++ src/main/kotlin/com/open592/appletviewer/environment/Architecture.kt
@@ -0,0 +1,21 @@
+package com.open592.appletviewer.environment
+
+/**
+ * The currently supported architectures.
+ */
+public enum class Architecture(private val needles: Set<String>) {
+    X86(setOf("x86", "i386")),
+    X86_64(setOf("x86_64", "amd64"));
+
+    public companion object {
+        public fun detect(osArch: String): Architecture {
+            return try {
+                Architecture.values().first {
+                    it.needles.contains(osArch)
+                }
+            } catch (_: NoSuchElementException) {
+                error("The following architecture is not supported: $osArch")
+            }
+        }
+    }
+}
