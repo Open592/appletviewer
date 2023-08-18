464fe70c396336cddb444396ed1236e585d95708
Open592 Developer <uss@open592.com>
2023-08-17 23:09:27 -0700

    config: Add a test to verify behavior for unknown hosts
    Signed-off-by: Open592 Developer <uss@open592.com>

--- src/test/kotlin/com/open592/appletviewer/config/resolver/JavConfigResolverTest.kt
+++ src/test/kotlin/com/open592/appletviewer/config/resolver/JavConfigResolverTest.kt
@@ -74,6 +74,29 @@ class JavConfigResolverTest {
     }
 
     @Test
+    fun `Should throw a LoadConfigurationException when no remote connection could be made`() {
+        val applicationPaths = mockk<ApplicationPaths>()
+        val preferences = mockk<AppletViewerPreferences>()
+        val settings = mockk<SystemPropertiesSettingsStore>()
+        val resolver = JavConfigResolver(preferences, applicationPaths, client, settings)
+
+        // Simulates when we attempt to make a connection to an invalid host
+        val configUrlTemplate = "https://invalid.connection/k=3/l=\$(Language:0)/jav_config.ws"
+
+        every { settings.getString("com.jagex.config") } returns configUrlTemplate
+        every { settings.getString("com.jagex.configfile") } returns ""
+        // Verify we can resolve a template param other than the default value
+        every { preferences.get("Language") } returns SupportedLanguage.GERMAN.getLanguageId().toString()
+
+        assertThrows<JavConfigResolveException.LoadConfigurationException> { resolver.resolve() }
+
+        verify(exactly = 1) { settings.getString("com.jagex.config") }
+        verify(exactly = 1) { settings.getString("com.jagex.configfile") }
+        // Need to resolve the URL template
+        verify(exactly = 1) { preferences.get("Language") }
+    }
+
+    @Test
     fun `Should throw a LoadConfigurationException when unable to fetch remote JavConfig file`() {
         val server = MockWebServer()
 
